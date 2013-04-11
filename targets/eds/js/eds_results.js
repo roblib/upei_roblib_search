c831b7fda79ee3922aa9d75fec639f5c49388f7b
Paul Pound <ppound@upei.ca>
2013-04-11 13:05:41 -0300

    first crack at eds results

--- targets/eds/js/eds_results.js
+++ targets/eds/js/eds_results.js
@@ -1,21 +1,23 @@
-Drupal.behaviors.roblib_search_cufts = {
+Drupal.behaviors.roblib_search_eds = {
     attach: function(context, settings) { 
         $url = settings.roblib_search_eds.search_url;
         jQuery.getJSON($url, function(data) {
             var items = [];     
             if(data.length < 1){
-            jQuery('#' + 'roblib-search-content-eds').empty().append('No Results');     
-        } else {
-            jQuery.each(data.journals, function(key, val) {
-                items.push('<div class ="roblib-search-row">');
-                items.push('<div class="roblib-title eds">');
-                items.push('<a href = "'+val.url+'">'+val.title+'</a></div>');
-                items.push('</div>');
-            });     
-        }            
-        jQuery('#' + 'roblib-search-content-eds').empty().append(items.join(''));
+                jQuery('#' + 'roblib-search-content-eds').empty().append('No Results');     
+            } else {
+                jQuery.each(data.records, function(key, val) {
+                    items.push('<div class ="roblib-search-row">');                    
+                    jQuery.each(val.RecordInfo.BibEntity.Titles, function(key2, val2){
+                         items.push('<div class="roblib-title eds">');
+                         items.push(val2.TitleFull+'</div>');
+                    })                   
+                    items.push('</div>');
+                });     
+            }            
+            jQuery('#' + 'roblib-search-content-eds').empty().append(items.join(''));
         });
-}
+    }
   
 }
 
