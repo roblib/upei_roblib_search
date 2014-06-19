2275ea44092e75e6298635e2db2170e62873188d
Paul Pound <ppound@upei.ca>
2014-06-19 14:57:47 -0300

    added hover pop up for cufts results

--- targets/cufts/js/cufts_results.js
+++ targets/cufts/js/cufts_results.js
@@ -13,48 +13,34 @@ Drupal.behaviors.roblib_search_cufts = {
                 jQuery('#' + 'roblib-search-content-cufts').empty().append('No Results');
                 jQuery('.' + 'pane-roblib-search-cufts-roblib-search-cufts-results').hide();
             } else {
+                var counter = 0;
+                var divs = new Array();
+                var content = new Array();
                 jQuery.each(data.journals, function(key, val) {
-                    var item_str = '<div class ="roblib-search-row">';
+                    id = 'roblib_cufts_results_' + counter;
+                    content[counter] = "";
+                    divs[counter] = id;
+                    var item_str = '<div class ="roblib-search-row" id="' + id + '">';
                     item_str += '<div class="roblib-title cufts">';
                     item_str += '<a href = "' + val.url + '">' + val.title + '</a>';
                     item_str += '</div>';
-
-                    //item_str += '<div class="cufts-issns">ISSN: ';
-                    //jQuery.each(val.issns, function(key2, val2) {
-                    //    var newIssn = "";
-                    //    var issn = val2.toString();
-                    //   if (issn.length == 8)
-                     //   {
-                     //       newIssn = issn.substr(0, 4) + '-' + issn.substr(4, 4);
-                      //  }
-                     //   else
-                      //  {
-                       //     newIssn = issn;
-                       // }
-                       // item_str += newIssn + ',';
-                    //});
-                    //item_str += '</div>';
-
-                    //item_str += '<div class="cufts-fulltext-coverages">';
-
-                    //jQuery.each(val.fulltext_coverages, function(key3, val3) {
-                    //    item_str += val3 + " ";
-                    //});
-
-                    //item_str += '</div>';
-
+                    jQuery.each(val.fulltext_coverages, function(key3, val3) {
+                        if(typeof val3 !== 'undefined' && val3 != null ){
+                            content[counter] += '<div class="cufts-coverage">' + val3 + "</div>";
+                        }
+                    });
+                    counter++;
                     item_str += '</div>';
                     items.push(item_str);
-
-
                 });
+                jQuery('#' + 'roblib-search-content-cufts').empty().append(items.join(''));
+                qtipify(divs, content, 'Coverage');
             }
             var originURL = settings.roblib_search_cufts.url;
             var originSuffix = settings.roblib_search_cufts.suffix;
             var newSuffix = originSuffix.replace("&format=json", "");
             if (parseInt(data.numOfResults) > 0)
             {
-                jQuery('#' + 'roblib-search-content-cufts').empty().append(items.join(''));
                 jQuery('#' + 'roblib-search-cufts-more').empty().append('<a href="' + originURL + newSuffix + '">see all ' + data.numOfResults + ' results</a>');
             }
             else
