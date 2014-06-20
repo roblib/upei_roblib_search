e41639a3356be90db0364a782b13805bbc8be8a0
Paul Pound <ppound@upei.ca>
2014-06-20 15:23:08 -0300

    added panels_preprocess function for each target to make theming easier

--- targets/solr_site/theme/roblib-search-solr-site-results.tpl.php
+++ targets/solr_site/theme/roblib-search-solr-site-results.tpl.php
@@ -9,6 +9,12 @@
  *
  */
 ?>
+<?
+// Get rid of warnings in admin interface
+if (!isset($type)){
+  $type = 'results';
+}
+?>
 <div class ="roblib-search-more" id="roblib-search-solr-site-<?php echo $type;?>-more"></div>
 <div class ="roblib-search-content solr-site-<?php echo $type;?>" id="roblib-search-content-solr-site-<?php echo $type;?>">
   <img src="<?php print (empty($spinner_path) ? ' ' : $spinner_path); ?>"/>
