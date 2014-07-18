b4e81be095a4f364bf245ea57e282b324d196c01
Paul Pound <ppound@upei.ca>
2014-07-18 11:08:53 -0300

    changes for bestbet and other sources

--- theme/roblib-search-other-sources.tpl.php
+++ theme/roblib-search-other-sources.tpl.php
@@ -13,7 +13,7 @@
      <a href="http://scholar.google.ca/scholar?q=<?php print ($query); ?>">Google Scholar</a>
   </div>
   <div class="roblib-search-row" id="roblib-search-google-cse-carleton">
-    <a href="http://www.google.com/cse?cx=015121753026627038989:9jvssbo3xgo&ie=UTF-8&q=<?php print ($query); ?>&sa=Search">Gov Docs Google CSE</a>
+    <a href="http://www.google.com/cse?cx=015121753026627038989:9jvssbo3xgo&ie=UTF-8&q=<?php print ($query); ?>&sa=Search">Canadian Government Sites</a>
   </div>
   <div class="roblib-search-row" id="roblib-search-filmsondemand">
     <a href="http://digital.films.com/PortalSearch.aspx?rd=lo&q=<?php print ($query); ?>">Films on Demand</a>
