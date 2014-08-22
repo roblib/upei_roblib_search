1ab1dd0ca7a848a779ccfd782974d1655255ec65
Paul Pound <ppound@upei.ca>
2014-08-22 13:41:10 -0300

    added reserves block refactored some of the solr search stuff

---
+++ targets/solr_site/js/solr_site_reserves.js
@@ -0,0 +1,42 @@
+Drupal.behaviors.roblib_search_solr_site_reserves = {
+  attach: function(context, settings) {
+    $url = settings.roblib_search_solr_site_reserves.search_url;
+    query = settings.roblib_search_solr_site_results.search_query;
+    baseUrl = settings.roblib_search_solr_site_results.base_url;
+    default_site_type = settings.roblib_search_solr_site_results.default_site_type + '/';
+    jQuery.getJSON($url, function(data) {
+      var items = [];
+      var numberOfDocs = 0;
+      try {
+        numberOfDocs = data.response.docs.length;
+      } catch (err) {
+        // do nothing leave docLength at 0
+      }
+      if (numberOfDocs < 1) {
+        jQuery('#' + 'roblib-search-content-solr-site-reserves').empty().append('No Results');
+          jQuery('.' + 'pane-roblib-search-solr-site-roblib-search-solr-site-reserves').hide();
+      } else {
+
+        jQuery('#' + 'roblib-search-content-solr-site-reserves').empty();
+          var counter = 0;
+          var divs = new Array();
+          var content = new Array();
+        jQuery.each(data.response.docs, function(key, val) {
+            id = 'roblib_search_solr_reserves_' + counter;
+            content[counter] = val.teaser;
+            divs[counter++] = id;
+         items.push('<div class ="roblib-search-row" id="' + id + '">\n\
+                        <div class="roblib-title reserves">\n\
+                          <a href="' + val.url + '">' + val.label + '</a></div></div>');
+        });
+        jQuery('#' + 'roblib-search-content-solr-site-reserves').empty().append(items.join(''));
+        qtipify(divs, content, 'Description');
+      }
+        if (numberOfDocs > 0)
+        {
+            jQuery('#' + 'roblib-search-solr-site-reserves-more').empty().append('<a href="'+ baseUrl + '/search/' + default_site_type + query +'?f[0]=bundle:course_reserve">see all results</a>');
+        }
+    });
+  }
+
+}
