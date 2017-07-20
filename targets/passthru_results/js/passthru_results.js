bbbd3675d1d683d69471b43be63ee4e92fc4d72c
Paul Pound <paul.pound@gmail.com>
2017-07-20 11:06:19 -0300

    added complete one search results to misc results

--- targets/passthru_results/js/passthru_results.js
+++ targets/passthru_results/js/passthru_results.js
@@ -2,6 +2,7 @@ Drupal.behaviors.roblib_search_passthru = {
   attach: function (context, settings) {
     scholar_url = settings.roblib_search_passthru.scholar_search_url;
     evergreen_url = settings.roblib_search_passthru.evergreen_search_url;
+    eds_url = settings.roblib_search_passthru.eds_search_url;
     jQuery.getJSON(scholar_url, function (data) {
      var scholar_numfound = data.response.numFound;
      var query = encodeURIComponent(data.responseHeader.params.q);
@@ -13,6 +14,12 @@ Drupal.behaviors.roblib_search_passthru = {
       var evergreen_numfound = data.numberOfRecords;
       jQuery('#roblib-search-passthru-evergreen-more').empty().append('<a href = "' + data.catalogBaseSearchUrl + '">PEI Collection (' +
         evergreen_numfound +')</a>');
+    });
+    jQuery.getJSON(eds_url, function (data) {
+      var eds_numfound = data.recordCount;
+      var href_str = 'http://search.ebscohost.com/login.aspx?direct=true&site=ehost-live&scope=site&type=1&custid=uprince&groupid=main&profid=eds&mode=bool&lang=en&bquery=';
+      jQuery('#roblib-search-passthru-eds-more').empty().append('<a href="http://proxy.library.upei.ca/login?url=' + href_str + query + '">Complete OneSearch results (' +
+        eds_numfound +')</a>');
     });
     jQuery('#roblib-search-content-passthru').empty();
   }
--- targets/passthru_results/theme/roblib-search-passthru.tpl.php
+++ targets/passthru_results/theme/roblib-search-passthru.tpl.php
@@ -2,23 +2,30 @@
 
 /**
  * @file roblib-search-passthru.tpl.php
-
  */
 ?>
 
-<div class ="roblib-search-content " id="roblib-search-content-passthru">
+<div class="roblib-search-content " id="roblib-search-content-passthru">
   <img src="<?php print (empty($spinner_path) ? ' ' : $spinner_path); ?>"/>
 </div>
-<div class ="roblib-search-row" id="roblib-search-passthru-islandscholar-more"></div>
-<div class ="roblib-search-row" id="roblib-search-passthru-evergreen-more"></div>
+<div class="roblib-search-row"
+     id="roblib-search-passthru-eds-more"></div>
+<div class="roblib-search-row"
+     id="roblib-search-passthru-islandscholar-more"></div>
+<div class="roblib-search-row" id="roblib-search-passthru-evergreen-more"></div>
 <div class="roblib-search-row" id="roblib-search-google-scholar">
-  <a href="http://scholar.google.ca/scholar?q=<?php print ($query); ?>">Google Scholar</a>
+  <a href="http://scholar.google.ca/scholar?q=<?php print ($query); ?>">Google
+    Scholar</a>
 </div>
 <div class="roblib-search-row" id="roblib-search-google-cse-carleton">
-  <a href="http://www.google.com/cse?cx=015121753026627038989:9jvssbo3xgo&ie=UTF-8&q=<?php print ($query); ?>&sa=Search">Canadian Government Sites</a>
+  <a
+    href="http://www.google.com/cse?cx=015121753026627038989:9jvssbo3xgo&ie=UTF-8&q=<?php print ($query); ?>&sa=Search">Canadian
+    Government Sites</a>
 </div>
 <div class="roblib-search-row" id="roblib-search-filmsondemand">
-  <a href="http://proxy.library.upei.ca/login?url=http://digital.films.com/portalplaylists.aspx?aid=14978&rd=lo&q=<?php print ($query); ?>">Films on Demand</a>
+  <a
+    href="http://proxy.library.upei.ca/login?url=http://digital.films.com/portalplaylists.aspx?aid=14978&rd=lo&q=<?php print ($query); ?>">Films
+    on Demand</a>
 </div>
 
 
